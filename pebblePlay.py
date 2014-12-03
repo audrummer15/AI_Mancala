aeb3a03e8b8ea72f3d89b998bbc4fe32624202bc
amh0029 <amh0029@tigermail.auburn.edu>
2014-12-02 18:18:20 -0600

    Progress
    Added transposition table functionality for alpha beta search.  Also,
    the min and max functions check to make sure they are checking valid
    moves (aka more than 0 pebbles in that slot).  Added computer vs
    computer functionality.  Added run through capability.

--- pebblePlay.py
+++ pebblePlay.py
@@ -15,6 +15,9 @@ from findertools import move
 PLAYER1 = 0  #player A
 PLAYER2 = 1  #player B
 
+# Global "Hash Table"
+transTable = {}
+
 #global variables
 depthForCutOffTest = 0
 
@@ -24,8 +27,54 @@ def heuristicFunction1(currentPlayer, gameBoardIn):
         totalPlayerPebs += gameBoardIn[currentPlayer][i]
     return totalPlayerPebs
 
+#Adam's function
+def utilityH2(currentPlayer, gameBoard):
+    playerPebbles = 0
+    opponentPebbles = 0
+
+    for i in range( len( gameBoard[currentPlayer] ) ):
+        playerPebbles += gameBoard[currentPlayer][i]
+        opponentPebbles += gameBoard[not(currentPlayer)][i]
+
+    return playerPebbles - opponentPebbles
+
+#########################################
+#                                        #
+#     Transposition Table Functionality   #
+#                                        #
+#########################################
+
+def boardToHashIndex(gameBoard, currentPlayer):
+    hashIndex1 = ""
+    hashIndex2 = ""
+
+    for i in range( len( gameBoard[PLAYER1] ) ):
+        hashIndex1 += str(gameBoard[PLAYER1][i])
+        hashIndex2 += str(gameBoard[PLAYER2][i])
+
+    return hashIndex1 + hashIndex2
+
+def getMinimaxFromHashTable(gameBoard):
+    hashIndex = boardToHashIndex(gameBoard)
+
+    if( hashIndex in transTable ):
+        returnValue = transTable[hashIndex]
+    else:
+        returnValue = False
+
+    return returnValue
+
+def storeMinimaxInHashTable(gameBoard, minimaxValue):
+    global transTable
+
+    if( getMinimaxFromHashTable(gameBoard) == False ):
+        hashIndex = boardToHashIndex(gameBoard)
+        transTable[hashIndex] = minimaxValue
+        return True
+    else:
+        return False
+
 def result(state, action, currentPlayer):
-    #tempState = list(state) #need to make a copy of gameBoard aka state
     numberOfColumns = len(state[0])
     tempState = [[state[x][y] for y in range(numberOfColumns)] for x in range(2)]
     makeMove(tempState, currentPlayer, action)
@@ -37,73 +86,82 @@ def cutOffTest(state, depth):
     else:
         return False
 
-def maxValue(state, alpha, beta, depth, pathStates, currentPlayer):
+def maxValue(state, alpha, beta, depth, pathStates, currentPlayer, heurValue, playerToMakeMove):
     if (state in pathStates):
         return float('-inf')
     if (cutOffTest(state, depth) == True):
         #check which utility is used
-        return heuristicFunction1(PLAYER1, state)
-    #pathStates.insert(state, len(pathStates))
+        if(heurValue == 1):
+            return heuristicFunction1(currentPlayer, state)
+        else:
+            return utilityH2(currentPlayer, state)
     pathStates.append(state)
     v = float('-inf')
     for move in range(len(state[0])):
-        statePrime = result(state, move, currentPlayer)
-        if False:#statePrime in transTable
-            #vPrime = transTable(statePrime)
-            return
-        else:
-            vPrime = minValue(statePrime, alpha, beta, (depth + 1), pathStates, not(currentPlayer))
-            #insert into transTable
-        if (vPrime > v):
-            v = vPrime
-        if (v >= beta):
-            return v
-        elif (v > alpha):
-            alpha = v
+        if(state[playerToMakeMove][move] > 0):
+            statePrime = result(state, move, playerToMakeMove)
+            if (getMinimaxFromHashTable(state) != False): #statePrime in transTable
+                vPrime = getMinimaxFromHashTable(state)
+                #return
+            else:
+                vPrime = minValue(statePrime, alpha, beta, (depth + 1), pathStates, currentPlayer, heurValue, not(playerToMakeMove))
+                storeMinimaxInHashTable(state, vPrime)
+            if (vPrime > v):
+                v = vPrime
+            if (v >= beta):
+                return v
+            elif (v > alpha):
+                alpha = v
     return v
             
     
-def minValue(state, alpha, beta, depth, pathStates, currentPlayer):
+def minValue(state, alpha, beta, depth, pathStates, currentPlayer, heurValue, playerToMakeMove):
     if (state in pathStates):
         return float('inf')
     if (cutOffTest(state, depth) == True):
         #check which utility is used
-        return heuristicFunction1(PLAYER1, state)
-    #pathStates.insert(state, len(pathStates))
+        if(heurValue == 1):
+            return heuristicFunction1(currentPlayer, state)
+        else:
+            return utilityH2(currentPlayer, state)
     pathStates.append(state)
     v = float('inf')
     for move in range(len(state[0])):
-        statePrime = result(state, move, currentPlayer)
-        if False:#statePrime in transTable
-            #vPrime = transTable(statePrime)
-            return 
-        else:
-            vPrime = maxValue(statePrime, alpha, beta, (depth + 1), pathStates, not(currentPlayer))
-            #insert into transTable
-        if (vPrime < v):
-            v = vPrime
-        if (v <= alpha):
-            return v
-        elif (v > beta):
-            beta = v
+        if(state[playerToMakeMove][move] > 0):
+            statePrime = result(state, move, playerToMakeMove)
+            if (getMinimaxFromHashTable(state) != False): #statePrime in transTable
+                vPrime = getMinimaxFromHashTable(state)
+                #return 
+            else:
+                vPrime = maxValue(statePrime, alpha, beta, (depth + 1), pathStates, currentPlayer, heurValue, not(playerToMakeMove))
+                storeMinimaxInHashTable(state, vPrime)
+            if (vPrime < v):
+                v = vPrime
+            if (v <= alpha):
+                return v
+            elif (v > beta):
+                beta = v
     return v
     
-def alphaBetaSearch(state, depth, currentPlayer): #state is game board and depth is the ply
+def alphaBetaSearch(state, depth, currentPlayer, heurValue): #state is game board and depth is the ply
+    playerToMakeMove = currentPlayer
     v = float('-inf')
     a = None
     alpha = float('-inf')
     beta = float('inf')
     pathStates = []
     for move in range(len(state[0])): #each column is a move
-        statePrime = result(state, move, currentPlayer)  #need to make fake gameboard?
-        vPrime = maxValue(statePrime, alpha, beta, depth, pathStates, currentPlayer) #had as min in algorithm, but shouldn't it be max?
-        if (vPrime > v):
-            v = vPrime
-            a = move
-        if (v >= beta):
-            return a
-        elif (v > alpha):
-            alpha = v
+        #if state is goal state
+        if(state[currentPlayer][move] > 0):
+            statePrime = result(state, move, currentPlayer)  #need to make fake gameboard?
+            vPrime = maxValue(statePrime, alpha, beta, depth, pathStates, currentPlayer, heurValue, playerToMakeMove) #had as min in algorithm, but shouldn't it be max?
+            if (vPrime > v):
+                v = vPrime
+                a = move
+            if (v >= beta):
+                return a
+            elif (v > alpha):
+                alpha = v
     return a
 
 def makeMove(gameBoardIn, currentPlayersMove, colToMoveFromIn):
@@ -214,99 +272,192 @@ def main():
     if(runOrStep == 1):
         notProperInput = True
         while(notProperInput):
-            userOrComp = int(input("Are you playing the game (enter 0) or watching the computer play itself(enter 1):  "))
-            if ((userOrComp == 0) or (userOrComp == 1)):
+            userPlaying = int(input("Is the computer playing itself (enter 0) or are you playing against the computer (enter 1)?:  "))
+            if ((userPlaying == 0) or (userPlaying == 1)):
                 notProperInput = False
+                
+    if(runOrStep == 0):  #means comp must be playing itself
+        userPlaying = 0
 
-    notProperInput = True
-    print "Heuristics"
-    print "1) Number of Pebbles of Player"
-    print "2) "
-    while(notProperInput):
-        hValue = int(input("Which heuristic do you want to use?  1 or 2:  "))
-        if((hValue == 1) or (hValue == 2)):
-            notProperInput = False
+    if(userPlaying):
+        notProperInput = True
+        print "Heuristics"
+        print "1) Number of Pebbles of Player"
+        print "2) Difference Between Your Pebbles and Opponent's Pebbles"
+        while(notProperInput):
+            hValue = int(input("Which heuristic do you want computer to use?  1 or 2:  "))
+            if((hValue == 1) or (hValue == 2)):
+                notProperInput = False
             
-    notProperInput = True
-    print "Planning Algorithms"
-    print "1) And-Or Search"
-    print "2) Alpha-Beta Minimax"
-    while(notProperInput):
-        algValue = int(input("Which algorithm do you want to use?  1 or 2:  "))
-        if((algValue == 1) or (algValue == 2)):
-            notProperInput = False
-    print ""
+        notProperInput = True
+        print "Planning Algorithms"
+        print "1) And-Or Search"
+        print "2) Alpha-Beta Minimax"
+        while(notProperInput):
+            algValue = int(input("Which algorithm do you want computer to use?  1 or 2:  "))
+            if((algValue == 1) or (algValue == 2)):
+                notProperInput = False
+        print ""
+        
+    else:
+        notProperInput = True
+        print "Heuristics"
+        print "1) Number of Pebbles of Player"
+        print "2) Difference Between Your Pebbles and Opponent's Pebbles"
+        while(notProperInput):
+            hValuePlayer1 = int(input("Which heuristic do you want to use for player 1?  1 or 2:  "))
+            if((hValuePlayer1 == 1) or (hValuePlayer1 == 2)):
+                notProperInput = False
+                
+        notProperInput = True
+        print "Heuristics"
+        print "1) Number of Pebbles of Player"
+        print "2) Difference Between Your Pebbles and Opponent's Pebbles"
+        while(notProperInput):
+            hValuePlayer2 = int(input("Which heuristic do you want to use for player 2?  1 or 2:  "))
+            if((hValuePlayer2 == 1) or (hValuePlayer2 == 2)):
+                notProperInput = False
+                
+        notProperInput = True
+        print "Planning Algorithms"
+        print "1) And-Or Search"
+        print "2) Alpha-Beta Minimax"
+        while(notProperInput):
+            algValuePlayer1 = int(input("Which algorithm do you want to use for player 1?  1 or 2:  "))
+            if((algValuePlayer1 == 1) or (algValuePlayer1 == 2)):
+                notProperInput = False
+        
+        notProperInput = True
+        print "Planning Algorithms"
+        print "1) And-Or Search"
+        print "2) Alpha-Beta Minimax"
+        while(notProperInput):
+            algValuePlayer2 = int(input("Which algorithm do you want to use for player 2?  1 or 2:  "))
+            if((algValuePlayer2 == 1) or (algValuePlayer2 == 2)):
+                notProperInput = False
+        print ""
 
     '''
     Set Up Game Board
     '''
     gameBoard = [[pebsPerSq for y in range(numCols)] for x in range(2)]  #range vs xrange?
     
-    if (runOrStep == 0):
-        print "TBD"
-    else:  
-        currentMove = PLAYER1
-        if (userOrComp == 0):
-            while(not(goalTest(gameBoard))):
-                '''
-                Use Function to Print Game Board
-                '''
-                printGameBoard(gameBoard)
-        
-                if(currentMove == PLAYER1):
-                    print ""
-                    print "Player 1's move"
-                    print ""
-                    
-                    if(algValue == 1):
-                        print "not implemented yet"
-                    else:
-                        colToMoveFrom = alphaBetaSearch(gameBoard, 0, PLAYER1)
-                        makeMove(gameBoard, PLAYER1, colToMoveFrom)
-                        debugString = "Algorithm returned:  "
-                        debugString += str(colToMoveFrom)
-                        print debugString
-                        print ""
-            
-                    currentMove = PLAYER2
-                
-                else:
-                    '''
-                    Calculates Player 2's Heuristic Value
-                    '''
-                    print ""
-                    player2String = "Assuming you are player 2, your heuristic value is "
-                    player2String += str(heuristicFunction1(PLAYER2, gameBoard))
-                    print player2String
-                    print ""
-        
-                    notValidInput = True
-                    while (notValidInput):
-                        colToMoveFrom = int(input("Which of your squares do you empty?  The numbering starts from the left with value 0:  "))
-                        if((colToMoveFrom < 0) or (colToMoveFrom >= numCols)):
-                            print "The column number is invalid.  Remember the numbering starts at 0."
-                        elif(gameBoard[PLAYER2][colToMoveFrom] == 0):
-                            print "This is an invalid move since you have no pebbles there."
-                        else:
-                            notValidInput = False
-                
-                    makeMove(gameBoard, PLAYER2, colToMoveFrom)
-            
-                    currentMove = PLAYER1
-            print ""
-            print "Final Game Board:"
-            
+    #if (runOrStep == 0): #when running through
+        #print "TBD"
+    #else:  #means stepping through
+    currentMove = PLAYER1
+    if(userPlaying):
+        while(not(goalTest(gameBoard))):
+            '''
+            Use Function to Print Game Board
+            '''
             printGameBoard(gameBoard)
-            
-            print ""
-            if(heuristicFunction1(PLAYER2, gameBoard) != 0):
-                print "Congratulations you won!"
-            else:
-                print "Sorry you lost. Better luck next time."
         
+            if(currentMove == PLAYER1):
+                print ""
+                print "Player 1's move"
+                print ""
+                    
+                if(algValue == 1):
+                    print "not implemented yet"
+                else:
+                    colToMoveFrom = alphaBetaSearch(gameBoard, 0, PLAYER1, hValue)
+                    makeMove(gameBoard, PLAYER1, colToMoveFrom)
+                    debugString = "Algorithm returned:  "
+                    debugString += str(colToMoveFrom)
+                    print debugString
+                    print ""
+            
+                currentMove = PLAYER2
+                
+            else:
+                notValidInput = True
+                while (notValidInput):
+                    colToMoveFrom = int(input("Which of your squares do you empty?  The numbering starts from the left with value 0:  "))
+                    if((colToMoveFrom < 0) or (colToMoveFrom >= numCols)):
+                        print "The column number is invalid.  Remember the numbering starts at 0."
+                    elif(gameBoard[PLAYER2][colToMoveFrom] == 0):
+                        print "This is an invalid move since you have no pebbles there."
+                    else:
+                        notValidInput = False
+                
+                makeMove(gameBoard, PLAYER2, colToMoveFrom)
+            
+                currentMove = PLAYER1
+        print ""
+        print "Final Game Board:"
+            
+        printGameBoard(gameBoard)
+            
+        print ""
+        if(heuristicFunction1(PLAYER2, gameBoard) != 0):
+            print "Congratulations you won!"
         else:
-            #step through with two computers
-            temp = 0
+            print "Sorry you lost. Better luck next time."
+        
+    else:
+        #step through with two computers or run with two computers
+        while(not(goalTest(gameBoard))):
+            '''
+            Use Function to Print Game Board
+            '''
+            printGameBoard(gameBoard)
+        
+            if(currentMove == PLAYER1):
+                print ""
+                print "Player 1's move"
+                print ""
+                    
+                if(algValuePlayer1 == 1):
+                    print "not implemented yet"
+                else:
+                    colToMoveFrom = alphaBetaSearch(gameBoard, 0, PLAYER1, hValuePlayer1)
+                    makeMove(gameBoard, PLAYER1, colToMoveFrom)
+                    debugString = "Algorithm returned:  "
+                    debugString += str(colToMoveFrom)
+                    print debugString
+                    print ""
+            
+                currentMove = PLAYER2
+                if(runOrStep == 1):
+                    notProperInput = True
+                    while(notProperInput):
+                        runOrStep = int(input("Would you like to run though the rest of the game or continue stepping through?  0-run through or 1-step through:  "))
+                        if ((runOrStep == 0) or (runOrStep == 1)):
+                            notProperInput = False
+                        
+            else:
+                print ""
+                print "Player 2's move"
+                print ""
+                    
+                if(algValuePlayer2 == 1):
+                    print "not implemented yet"
+                else:
+                    colToMoveFrom = alphaBetaSearch(gameBoard, 0, PLAYER2, hValuePlayer2)
+                    makeMove(gameBoard, PLAYER2, colToMoveFrom)
+                    debugString = "Algorithm returned:  "
+                    debugString += str(colToMoveFrom)
+                    print debugString
+                    print ""
+            
+                currentMove = PLAYER1
+                if(runOrStep == 1):
+                    notProperInput = True
+                    while(notProperInput):
+                        runOrStep = int(input("Would you like to run though the rest of the game or continue stepping through?  0-run through or 1-step through:  "))
+                        if ((runOrStep == 0) or (runOrStep == 1)):
+                            notProperInput = False
+        print ""
+        print "Final Game Board:"
+            
+        printGameBoard(gameBoard)
+            
+        print ""
+        if(heuristicFunction1(PLAYER2, gameBoard) != 0):
+            print "Player 2 won!"
+        else:
+            print "Player 1 won!"
     
     return
 
